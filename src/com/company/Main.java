af70fda33ac0500916be7aa29df6016d026abe6e
YuraMihailov123 <recon.97@mail.ru>
2018-06-30 13:11:30 +0300

    Divided on classes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,209 +1,65 @@
 package com.company;
-import  java.util.Scanner;
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
 import java.io.*;
+import java.util.Scanner;
+import static com.company.ActionListenersClass.*;
+import static com.company.AlgorithmClass.*;
+import static com.company.InitializeClass.*;
 
 public class Main extends JFrame{
-
-    //private String[][] _words;
-    private String[] _pairs = new String[32];
-    private int _buttonPressedNumber, _iterator =0,_countPairs=0;;
-    boolean _isFirst =false,_isGaming=false;
-    private JLabel _emptyLabel;
-    private JButton[] _buttons=new JButton[64];
-    private JButton _gameButton,_alphabetButton,_startButton;
-    private  File file;
-    private FileReader rf;
-    private  Scanner scan;
-    //private int finalI=0;
-    //private JLayeredPane lpane;
+    /*
+    -начать с перемещ и без
+    -сохранить список ошибок по кнопке(2 списка по раздельно)
+    -задать количество ячеек по гориз и верт и их размеры
+    -ключи
+    */
+    static String[] _pairs = new String[32];
+    static int _buttonPressedNumber, _iterator =0,_countPairs=0, _buttonNumberBackToWhite;
+    static boolean _isFirst =false,_isGaming=false;
+    static JLabel _timeLabel;
+    static JLabel _emptyLabel;
+    static JLabel _scoreLabel;
+    static JLabel _errorLabel;
+    static JButton[] _buttons=new JButton[64];
+    private static JButton _gameButton,_alphabetButton;
+    static int _score=0,_error=0;
+    public static File file;
+    static FileReader rf;
+    static Scanner scan;
+    static Container _container;
+    static int[] timerGame = {0,0,0};
+    static final Timer[] timer = new Timer[1];
+    static final Timer[] timer3 = new Timer[1];
+    static final Timer[] timer2= new Timer[1];
     private Main(){
         super("Memory training"); //Заголовок окна
         setResizable(false);
         setBounds(400, 0, 800, 860);
 
-        /*String path = "icon.jpg";
-        URL imgUrl = Main.class.getResource(path);
-        //icon = Toolkit.getDefaultToolkit().getImage("com.company/icon.jpg");
-        ImageIcon icon = new ImageIcon(imgUrl);
-        */
-        //lpane=new JLayeredPane();
-
-        Container _container = getContentPane();
+        _container = getContentPane();
         _container.setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
 
+        _emptyLabel = JLabel(_emptyLabel,"DMG Group. 2018. recon.97@mail.ru -beta 0.1v.",4,300,30);
+        _scoreLabel = JLabel(_scoreLabel,"Очки: "+_score,0,100,30);
+        _errorLabel = JLabel(_errorLabel,"Ошибки: "+_error,0,100,30);
+        _timeLabel = JLabel(_timeLabel,"Время: ",0,100,30);
 
-        _emptyLabel = new JLabel("DMG Group. 2018. recon.97@mail.ru -beta 0.1v.");
-        _emptyLabel.setHorizontalAlignment(JLabel.RIGHT);
-        _gameButton=new JButton("Игра");
-        _alphabetButton=new JButton("Алфавит");
-        _gameButton.setPreferredSize(new Dimension(100,30));
-        _alphabetButton.setPreferredSize(new Dimension(100,30));
-        _gameButton.setBackground(Color.WHITE);
-        _alphabetButton.setBackground(Color.WHITE);
-        _emptyLabel.setPreferredSize(new Dimension(600,30));
+        _gameButton = JButton(_gameButton,"Игра",100,30);
+        _alphabetButton = JButton(_alphabetButton,"Алфавит",100,30);
+
         _container.add(_gameButton);
         _container.add(_alphabetButton);
+        _container.add(_scoreLabel);
+        _container.add(_errorLabel);
+        _container.add(_timeLabel);
         _container.add(_emptyLabel);
-/////////////////////////////////////////////////////////////////////
-        final Timer[] timer = new Timer[1];
-        ActionListener listener = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //for (int _iterator = 0; _iterator <64 ; _iterator++) {
-                if(_iterator <32) {
-                    //_buttons[_iterator].setText("_iterator");
-                    //GenerateLevel();
-                    OpenFile();
-                    _iterator++;
-                }else {
-                    timer[0].stop();
-                    for (int i = 0; i < 64; i++) {
-                        _buttons[i].setEnabled(true);
-                    }
-                    _isGaming=true;
 
-                }
-                //}
-            }
-        };
+        _gameButton.addActionListener(startGame);
 
-        _gameButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-
-                try{
-                    //OpenFile();
-                    rf = new FileReader("src/com/company/file.txt");
-                    scan = new Scanner(rf);
-                }
-                catch (FileNotFoundException e1) {
-                    System.out.println("File not found!");
-                }
-
-                timer[0] = new Timer(100,listener);
-                timer[0].start();
-            }
-        });
-///////////////////////////////////////////////////////////////////////
-        for (int i = 0; i < 64; i++) {
-            //char a = _iterator
-            _buttons[i]=new JButton("");
-            _buttons[i].setBackground(Color.GREEN);
-            _buttons[i].setEnabled(false);
-            _buttons[i].setPreferredSize(new Dimension(100,100));
-            _container.add(_buttons[i]);
-        }
-/////////////////////////////////////////////////////////
-
-        final Timer[] timer2= new Timer[1];
-
-        ActionListener listener2 = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //_buttons[finalI].setBackground(Color.RED);
-                _buttons[_buttonPressedNumber].setBackground(Color.RED);
-                timer2[0].stop();
-            }
-        };
-
-
-            for (int i = 0; i < 64; i++) {
-
-                int finalI = i;
-                _buttons[i].addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-                        if(_isGaming) {
-                            if (!_isFirst) {
-                                _isFirst = !_isFirst;
-                                _buttonPressedNumber = finalI;
-                                _buttons[finalI].setBackground(Color.YELLOW);
-                            } else if (_isFirst && _buttonPressedNumber != finalI) {
-                                String temp1, temp2;
-                                temp1 = _buttons[finalI].getText() + " " + _buttons[_buttonPressedNumber].getText();
-                                temp2 = _buttons[_buttonPressedNumber].getText() + " " + _buttons[finalI].getText();
-                                for (int j = 0; j < _pairs.length; j++) {
-                                    //System.out.println(_pairs[j].equals(temp2));
-                                    if (_pairs[j].equals(temp2) || _pairs[j].equals(temp1)) {
-                                        _buttons[finalI].setBackground(Color.GREEN);
-                                        _buttons[_buttonPressedNumber].setBackground(Color.GREEN);
-                                        _buttons[finalI].setEnabled(false);
-                                        _buttons[_buttonPressedNumber].setEnabled(false);
-                                        _buttons[finalI].setText("");
-                                        _buttons[_buttonPressedNumber].setText("");
-                                        break;
-                                    } else {
-                                        timer2[0] = new Timer(1000, listener2);
-                                        timer2[0].start();
-
-                                    }
-                                    System.out.println("break??");
-                                }
-
-                                _isFirst = !_isFirst;
-                            } else if (_buttonPressedNumber == finalI) {
-                                _buttons[finalI].setBackground(Color.WHITE);
-                                _isFirst = !_isFirst;
-                            }
-
-                        }
-                    }
-                });
-
-        }
-        //GenerateLevel();
-        //_buttons[0]=_buttons[32];
-
+        InitializeButtons();
+        ActionButton();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-
-    /*void GenerateLevel(){
-        try{
-            OpenFile();
-
-        }
-        catch (FileNotFoundException e) {
-            System.out.println("File not found!");
-        }
-
-    }*/
-
-    public void OpenFile() {//} throws FileNotFoundException {
-
-        String temp1,temp2;
-
-        String str="";
-        str = scan.nextLine();
-        _pairs[_countPairs] = str;
-        //here while
-        if(scan.hasNextLine() && _countPairs<32){
-            temp1="";
-            temp2="";
-           int _temp = SubStringOnParts(str);
-            temp1 = str.substring(0, _temp);
-            _buttons[_countPairs].setText(temp1);
-            temp2 = str.substring(_temp + 1);
-            _buttons[_countPairs+32].setText(temp2);
-            _buttons[_countPairs].setBackground(Color.WHITE);
-            _buttons[_countPairs+32].setBackground(Color.WHITE);
-
-            //str = scan.nextLine();
-            _countPairs++;
-
-        }
-        //rf.close();
-    }
-
-    int SubStringOnParts(String str){
-        //String temp="";
-        for (int i = 0; i < str.length(); i++) {
-            if(str.charAt(i)==' '){
-                return i;
-                //System.out.println("fuck");
-            }
-        }
-        return 0;
     }
 
     public static void main(String[] args) {
